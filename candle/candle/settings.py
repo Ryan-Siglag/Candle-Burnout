9b20a4fe947795a2a6dac0e8793ab5f2fcbe02a2
Ryan-Siglag <ryan.siglag@gmail.com>
2026-03-31 23:03:59 -0400

    Allowed hosts edit

--- candle/candle/settings.py
+++ candle/candle/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['backend', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['192.168.1.168', 'localhost', '127.0.0.1']
 
 # Application definition
 
